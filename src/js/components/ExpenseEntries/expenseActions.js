b400132dbdb3a2881d5a95ef6fbf94e776a5b905
ccficorilli <ccficorilli@gmail.com>
2018-06-11 21:25:26 -0700

    everything is working, math is fixed

--- src/js/components/ExpenseEntries/expenseActions.js
+++ src/js/components/ExpenseEntries/expenseActions.js
@@ -1,23 +1,27 @@
 export function updateExpenseDescription(description) {
-    return {
-        type: 'UPDATE_EXPENSE_DESCRIPTION',
-        payload: { description }
-    };
+  return {
+    type: "UPDATE_EXPENSE_DESCRIPTION",
+    payload: {
+      description
+    }
+  };
 }
 
 export function updateExpenseAmount(amount) {
-    return {
-        type: 'UPDATE_EXPENSE_AMOUNT',
-        payload: { amount }
-    };
+  return {
+    type: "UPDATE_EXPENSE_AMOUNT",
+    payload: {
+      amount
+    }
+  };
 }
 
 export function addExpense(description, amount) {
-    return {
-        type: 'ADD_EXPENSE',
-        payload: {
-            description,
-            amount: parseFloat(amount)
-        }
-    };
+  return {
+    type: "ADD_EXPENSE",
+    payload: {
+      description,
+      amount: parseFloat(amount)
+    }
+  };
 }
--- src/js/components/Summary/Summary.jsx
+++ src/js/components/Summary/Summary.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function calculateSum(lineItems) {
   return lineItems.reduce((acc, lineItem) => acc + lineItem.amount, 0);
@@ -7,42 +7,47 @@ function calculateSum(lineItems) {
 function formatCurrency(amount) {
   if (amount >= 0) {
     const dollars = Math.floor(amount);
-    const cents = Math.floor((amount - dollars) * 100).toString().padEnd(2, '0');
+    const cents = Math.floor((amount - dollars) * 100)
+      .toString()
+      .padEnd(2, "0");
     return `$${dollars.toLocaleString()}.${cents}`;
   }
 
-  const dollars = Math.ceil(amount);
-  const cents = Math.floor((amount - dollars) * 100 * -1).toString().padEnd(2, '0');
-  return `-$${(dollars * -1).toLocaleString()}.${cents}`;
+  // const dollars = Math.ceil(amount);
+  // const cents = Math.floor((amount - dollars) * 100 * -1)
+  //   .toString()
+  //   .padEnd(2, "0");
+  // return `-$${(dollars * -1).toLocaleString()}.${cents}`;
 }
 
 class Summary extends React.Component {
   render() {
     const { incomeItems, expenseItems } = this.props;
 
-    const incomeTotal = calculateSum(incomeItems) / 100;
-    const expenseTotal = calculateSum(expenseItems) / 100;
-    const difference = Math.round(incomeTotal - expenseTotal) / 100;
+    const incomeTotal = calculateSum(incomeItems);
+    const expenseTotal = calculateSum(expenseItems);
+    const difference = (Math.round((incomeTotal - expenseTotal) * 100) / 100
+    ).toFixed(2);
 
     return (
-      <div className='card border-info mb-3'>
-        <div className='card-header text-white bg-info'>Summary</div>
-        <div className='card-body'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-6 text-center'>
-                <h6 className='h6 strong'>Total Income</h6>
-                <p>{ formatCurrency(incomeTotal) }</p>
+      <div className="card border-info mb-3">
+        <div className="card-header text-white bg-info">Summary</div>
+        <div className="card-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-6 text-center">
+                <h6 className="h6 strong">Total Income</h6>
+                <p>{formatCurrency(incomeTotal)}</p>
               </div>
-              <div className='col-6 text-center'>
-                <h6 className='h6 strong'>Total Expenses</h6>
-                <p>{ formatCurrency(expenseTotal) }</p>
+              <div className="col-6 text-center">
+                <h6 className="h6 strong">Total Expenses</h6>
+                <p>{formatCurrency(expenseTotal)}</p>
               </div>
             </div>
-            <div className='row justify-content-center'>
-              <div className='col-6 text-center'>
-                <h6 className='h6 strong'>Left after spending</h6>
-                <p>{ formatCurrency(difference) }</p>
+            <div className="row justify-content-center">
+              <div className="col-6 text-center">
+                <h6 className="h6 strong">Left after spending</h6>
+                <p>{difference}</p>
               </div>
             </div>
           </div>
