5d403470d7f6f1ea79df09d902654f5c98d49e55
ccficorilli <ccficorilli@gmail.com>
2018-06-11 21:37:57 -0700

    adding circle ci tests

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
+
 import App from './../src/js/app';
 
 describe('App Component', () => {
